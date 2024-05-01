df8f7973600290f967bf254d1f9b67f0da1e5601
hafei <zhangmengcoding@gmail.com>
2024-05-01 23:56:06 +0800

    Initial commit for shared code library with log4net integration.

---
+++ poc.log4net.shared/SharedClass.cs
@@ -0,0 +1,21 @@
+﻿using log4net;
+
+namespace poc.log4net.shared
+{
+    public class SharedClass
+    {
+        private  static readonly ILog log = LogManager.GetLogger(typeof(SharedClass));
+        //generated code
+        public SharedClass()
+        {
+        }
+        public void DoSomething()
+        {
+            log.Info("do something!");
+        }
+        public void DoSomethingElse()
+        {
+            log.Info("do something else!");
+        }
+    }
+}
