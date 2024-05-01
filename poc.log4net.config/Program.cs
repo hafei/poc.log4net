beb3f5f797994375975755680aa7718fcfd127a8
hafei <zhangmengcoding@gmail.com>
2024-05-01 23:59:36 +0800

    Add log4net config and test program.

---
+++ poc.log4net.config/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace poc.log4net.config
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
--- poc.log4net/Program.cs
+++ poc.log4net/Program.cs
@@ -6,27 +6,32 @@ using poc.log4net.shared;
 
 namespace poc.log4net
 {
-    internal class Program
+    class Program
     {  
+        /// <summary>
+        /// 静态构造函数用于设置日志配置。
+        /// </summary>
         static Program()
         {
-            // 设置配置
+            // 设置日志布局格式
             var repository = LogManager.GetRepository(System.Reflection.Assembly.GetEntryAssembly());
             var patternLayout = new PatternLayout
             {
-                ConversionPattern = "%date{ABSOLUTE} %level %logger - %message%newline"
+                ConversionPattern = "%date{ABSOLUTE} %level %logger - %message%newline" // 定义日志输出格式
             };
-            patternLayout.ActivateOptions();
+            patternLayout.ActivateOptions(); // 激活日志布局配置
 
+            // 配置将日志输出到控制台的Appender
             var consoleAppender = new ConsoleAppender
             {
-                Layout = patternLayout,
+                Layout = patternLayout, // 设置日志布局
             };
-            consoleAppender.ActivateOptions();
+            consoleAppender.ActivateOptions(); // 激活控制台Appender配置
 
-            // 添加控制台Appender到root logger
+            // 将控制台Appender添加到根Logger
             BasicConfigurator.Configure(repository, consoleAppender);
         }
+
         
         private static readonly ILog log = LogManager.GetLogger(typeof(Program));
         public static void Main(string[] args)
